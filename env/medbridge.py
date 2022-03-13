d0c83f407e0ddd64c43a5decd53720a0767a067b
Madhav Menon <madhavmenon1007@gmail.com>
2022-03-13 10:16:22 +0530

    Finish app

--- env/medbridge.py
+++ env/medbridge.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from forms import PatientEntry
+import psycopg2 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "4a0f2618ecf89b6e57bc979ec5b246bbbb45a9d332f4a0510b1607bbc0b064c4" # Makes our app more secure
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Pookie10_@localhost/patientinformation" 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Pookie10_@localhost/patientdetails" 
 
 db = SQLAlchemy(app) # Creates database
 
@@ -12,7 +13,7 @@ class Patient(db.Model):
     patient_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable = False)
     age = db.Column(db.Integer, nullable=False)
-    gender = db.Column(db.String, nullable=False)
+    patient_sex = db.Column(db.String, nullable=False)
     blood_type = db.Column(db.String(3), nullable=False)
     injury_type = db.Column(db.String, nullable=False)
     injury_location = db.Column(db.String, nullable=False)
@@ -22,11 +23,11 @@ class Patient(db.Model):
     attenders_code = db.Column(db.String, nullable=False)
     ambulance_licenceplate = db.Column(db.String, nullable = False)
 
-    def __init__(self, patient_id, name, age, gender, blood_type, injury_type, injury_location, injury_cause, heart_rate, o2_level, attenders_code, ambulance_licenceplate):
+    def __init__(self, patient_id, name, age, patient_sex, blood_type, injury_type, injury_location, injury_cause, heart_rate, o2_level, attenders_code, ambulance_licenceplate):
         self.patient_id = patient_id
         self.name = name
         self.age = age
-        self.gender = gender
+        self.patient_sex = patient_sex
         self.blood_type = blood_type
         self.injury_type = injury_type
         self.injury_location = injury_location
@@ -39,7 +40,8 @@ class Patient(db.Model):
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", title = "Medbridge")
+    flask_form = PatientEntry()
+    return render_template("patient_entry.html", title = "Medbridge", form=flask_form)
 
 @app.route("/about")
 def about():
@@ -55,11 +57,11 @@ def patient_entry():
 
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
-    id= 1
+    id = 10
     if request.method == "POST":
         name = request.form["name"]
         age = request.form["age"]
-        gender = request.form["gender"]
+        patient_sex = request.form["patient_sex"]
         blood_type = request.form["blood_type"]
         injury_type = request.form["injury_type"]
         injury_location = request.form["injury_location"]
@@ -68,11 +70,13 @@ def submit():
         o2_level = request.form["o2_level"]
         attenders_code = request.form["attenders_code"]
         ambulance_licenceplate = request.form["ambulance_licenceplate"]
-        patient = Patient(patient_id = id, name = name, age = age, gender = gender, blood_type = blood_type, injury_type = injury_type, injury_location = injury_location, injury_cause = injury_cause, heart_rate = heart_rate, o2_level = o2_level, attenders_code = attenders_code, ambulance_licenceplate = ambulance_licenceplate)
+        patient = Patient(patient_id = id,name = name, age = age, patient_sex = patient_sex, blood_type = blood_type, injury_type = injury_type, injury_location = injury_location, injury_cause = injury_cause, heart_rate = heart_rate, o2_level = o2_level, attenders_code = attenders_code, ambulance_licenceplate = ambulance_licenceplate)
         db.session.add(patient)
         db.session.commit()
         id+=1
+    
     return render_template("success.html")
+     
 
 
 # @app.route("/registration")
